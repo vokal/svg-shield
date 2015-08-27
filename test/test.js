27b37ace69411d0abd70705720c2796feebf4d5d
foresmac <foresmac@gmail.com>
2015-08-27 17:38:31 -0500

    Update tests

--- test/test.js
+++ test/test.js
@@ -13,12 +13,12 @@ describe( "svg-shield", function ()
         {
             assert.equal( err, null );
             assert.equal( res, '<svg xmlns="http://www.w3.org/2000/svg" width="130" height="20">\n\
-    <mask id="a"><rect width="130" height="20" rx="3" fill="#fff"/></mask>\n\
+    <mask id="a"><rect width="130" height="20" rx="5" ry="5" fill="#fff"/></mask>\n\
     <g mask="url(#a)">\n\
         <path fill="#555" d="M0 0h90v20H0z"/>\n\
         <path fill="#4b1" d="M90 0h60v20H90z"/>\n\
     </g>\n\
-    <g fill="#fff" text-anchor="middle" font-family="sans-serif" font-size="11">\n\
+    <g fill="#fff" text-anchor="middle" font-family="Verdana, sans-serif" font-size="12">\n\
         <text x="45" y="15" fill="#000"></text>\n\
         <text x="110" y="15" fill="#000"></text>\n\
         <text x="45" y="14"></text>\n\
@@ -46,12 +46,12 @@ describe( "svg-shield", function ()
             assert.equal( err, null );
             assert.equal( res,
             '<svg xmlns="http://www.w3.org/2000/svg" width="150" height="20">\n\
-    <mask id="a"><rect width="150" height="20" rx="3" fill="#fff"/></mask>\n\
+    <mask id="a"><rect width="150" height="20" rx="5" ry="5" fill="#fff"/></mask>\n\
     <g mask="url(#a)">\n\
         <path fill="#333" d="M0 0h100v20H0z"/>\n\
         <path fill="#000" d="M100 0h60v20H100z"/>\n\
     </g>\n\
-    <g fill="#fff" text-anchor="middle" font-family="sans-serif" font-size="11">\n\
+    <g fill="#fff" text-anchor="middle" font-family="Verdana, sans-serif" font-size="12">\n\
         <text x="50" y="15" fill="#000">name</text>\n\
         <text x="125" y="15" fill="#000">value</text>\n\
         <text x="50" y="14">name</text>\n\
