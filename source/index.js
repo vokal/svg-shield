de52c1fe87e1b7d1b1bccef1e4036e422f7a5c50
foresmac <foresmac@gmail.com>
2015-08-27 17:34:57 -0500

    Minor visual tweaks
    - Make the type slightly larger
    - Give the badge a larger chamfer radius
    - Verdana seems to look better than default Helvetica, maybe because
    most badges use Verdana?

--- source/index.js
+++ source/index.js
@@ -5,12 +5,12 @@ var mustache = require( "mustache" );
 
 var shield = {
     svgData: '<svg xmlns="http://www.w3.org/2000/svg" width="{{width}}" height="20">\n\
-    <mask id="a"><rect width="{{width}}" height="20" rx="3" fill="#fff"/></mask>\n\
+    <mask id="a"><rect width="{{width}}" height="20" rx="5" ry="5" fill="#fff"/></mask>\n\
     <g mask="url(#a)">\n\
         <path fill="{{nameBgColor}}" d="M0 0h{{nameWidth}}v20H0z"/>\n\
         <path fill="{{valueBgColor}}" d="M{{nameWidth}} 0h60v20H{{nameWidth}}z"/>\n\
     </g>\n\
-    <g fill="#fff" text-anchor="middle" font-family="sans-serif" font-size="11">\n\
+    <g fill="#fff" text-anchor="middle" font-family="Verdana, sans-serif" font-size="12">\n\
         <text x="{{nameCenter}}" y="15" fill="#000">{{name}}</text>\n\
         <text x="{{valueCenter}}" y="15" fill="#000">{{value}}</text>\n\
         <text x="{{nameCenter}}" y="14">{{name}}</text>\n\
