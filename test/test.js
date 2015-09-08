041ce83a0167e1a3dfe93b6928159c5169e73cee
foresmac <foresmac@gmail.com>
2015-09-08 16:18:19 -0500

    Update tests per @jrit

--- test/test.js
+++ test/test.js
@@ -79,21 +79,13 @@ describe( "svg-shield", function ()
     it( "should get a shield with max 17 font-size", function ( done )
     {
         var options = {
-            valueWidth: 50,
-            nameWidth: 100,
-            valueBgColor: "#000",
-            nameBgColor: "#333",
-            name: "name",
-            value: "value",
-            fontFamily: "Verdana, sans-serif",
             fontSize: 20
         };
 
         shield.getShield( options, function ( err, res )
         {
             assert.equal( err, null );
-            assert.equal( res,
-            '<svg xmlns="http://www.w3.org/2000/svg" width="150" height="20">\n\
+            assert.equal( res, '<svg xmlns="http://www.w3.org/2000/svg" width="130" height="20">\n\
     <defs>\n\
         <filter id="drop-shadow">\n\
             <feOffset result="offOut" in="SourceAlpha" dx="0.5" dy="1" />\n\
@@ -101,14 +93,14 @@ describe( "svg-shield", function ()
             <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />\n\
         </filter>\n\
     </defs>\n\
-    <mask id="round-rect"><rect width="150" height="20" rx="5" ry="5" fill="#fff"/></mask>\n\
+    <mask id="round-rect"><rect width="130" height="20" rx="5" ry="5" fill="#fff"/></mask>\n\
     <g mask="url(#round-rect)">\n\
-        <path fill="#333" d="M0 0h100v20H0z"/>\n\
-        <path fill="#000" d="M100 0h60v20H100z"/>\n\
+        <path fill="#555" d="M0 0h90v20H0z"/>\n\
+        <path fill="#4b1" d="M90 0h60v20H90z"/>\n\
     </g>\n\
-    <g fill="#fff" text-anchor="middle" font-family="Verdana, sans-serif" font-size="17">\n\
-        <text x="50" y="14" filter="url(#drop-shadow)">name</text>\n\
-        <text x="125" y="14" filter="url(#drop-shadow)">value</text>\n\
+    <g fill="#fff" text-anchor="middle" font-family="sans-serif" font-size="17">\n\
+        <text x="45" y="14" filter="url(#drop-shadow)"></text>\n\
+        <text x="110" y="14" filter="url(#drop-shadow)"></text>\n\
     </g>\n\
 </svg>' );
 
@@ -119,21 +111,13 @@ describe( "svg-shield", function ()
     it( "should get a shield with min 8 font-size", function ( done )
     {
         var options = {
-            valueWidth: 50,
-            nameWidth: 100,
-            valueBgColor: "#000",
-            nameBgColor: "#333",
-            name: "name",
-            value: "value",
-            fontFamily: "Verdana, sans-serif",
             fontSize: 4
         };
 
         shield.getShield( options, function ( err, res )
         {
             assert.equal( err, null );
-            assert.equal( res,
-            '<svg xmlns="http://www.w3.org/2000/svg" width="150" height="20">\n\
+            assert.equal( res, '<svg xmlns="http://www.w3.org/2000/svg" width="130" height="20">\n\
     <defs>\n\
         <filter id="drop-shadow">\n\
             <feOffset result="offOut" in="SourceAlpha" dx="0.5" dy="1" />\n\
@@ -141,14 +125,14 @@ describe( "svg-shield", function ()
             <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />\n\
         </filter>\n\
     </defs>\n\
-    <mask id="round-rect"><rect width="150" height="20" rx="5" ry="5" fill="#fff"/></mask>\n\
+    <mask id="round-rect"><rect width="130" height="20" rx="5" ry="5" fill="#fff"/></mask>\n\
     <g mask="url(#round-rect)">\n\
-        <path fill="#333" d="M0 0h100v20H0z"/>\n\
-        <path fill="#000" d="M100 0h60v20H100z"/>\n\
+        <path fill="#555" d="M0 0h90v20H0z"/>\n\
+        <path fill="#4b1" d="M90 0h60v20H90z"/>\n\
     </g>\n\
-    <g fill="#fff" text-anchor="middle" font-family="Verdana, sans-serif" font-size="8">\n\
-        <text x="50" y="14" filter="url(#drop-shadow)">name</text>\n\
-        <text x="125" y="14" filter="url(#drop-shadow)">value</text>\n\
+    <g fill="#fff" text-anchor="middle" font-family="sans-serif" font-size="8">\n\
+        <text x="45" y="14" filter="url(#drop-shadow)"></text>\n\
+        <text x="110" y="14" filter="url(#drop-shadow)"></text>\n\
     </g>\n\
 </svg>' );
 
@@ -159,22 +143,13 @@ describe( "svg-shield", function ()
     it( "should get a shield with min 0 corner radius", function ( done )
     {
         var options = {
-            valueWidth: 50,
-            nameWidth: 100,
-            valueBgColor: "#000",
-            nameBgColor: "#333",
-            name: "name",
-            value: "value",
-            fontFamily: "Verdana, sans-serif",
-            fontSize: 12,
             radius: -6
         };
 
         shield.getShield( options, function ( err, res )
         {
             assert.equal( err, null );
-            assert.equal( res,
-            '<svg xmlns="http://www.w3.org/2000/svg" width="150" height="20">\n\
+            assert.equal( res, '<svg xmlns="http://www.w3.org/2000/svg" width="130" height="20">\n\
     <defs>\n\
         <filter id="drop-shadow">\n\
             <feOffset result="offOut" in="SourceAlpha" dx="0.5" dy="1" />\n\
@@ -182,14 +157,14 @@ describe( "svg-shield", function ()
             <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />\n\
         </filter>\n\
     </defs>\n\
-    <mask id="round-rect"><rect width="150" height="20" rx="0" ry="0" fill="#fff"/></mask>\n\
+    <mask id="round-rect"><rect width="130" height="20" rx="0" ry="0" fill="#fff"/></mask>\n\
     <g mask="url(#round-rect)">\n\
-        <path fill="#333" d="M0 0h100v20H0z"/>\n\
-        <path fill="#000" d="M100 0h60v20H100z"/>\n\
+        <path fill="#555" d="M0 0h90v20H0z"/>\n\
+        <path fill="#4b1" d="M90 0h60v20H90z"/>\n\
     </g>\n\
-    <g fill="#fff" text-anchor="middle" font-family="Verdana, sans-serif" font-size="12">\n\
-        <text x="50" y="14" filter="url(#drop-shadow)">name</text>\n\
-        <text x="125" y="14" filter="url(#drop-shadow)">value</text>\n\
+    <g fill="#fff" text-anchor="middle" font-family="sans-serif" font-size="11">\n\
+        <text x="45" y="14" filter="url(#drop-shadow)"></text>\n\
+        <text x="110" y="14" filter="url(#drop-shadow)"></text>\n\
     </g>\n\
 </svg>' );
 
@@ -200,22 +175,13 @@ describe( "svg-shield", function ()
     it( "should get a shield with max 10 corner radius", function ( done )
     {
         var options = {
-            valueWidth: 50,
-            nameWidth: 100,
-            valueBgColor: "#000",
-            nameBgColor: "#333",
-            name: "name",
-            value: "value",
-            fontFamily: "Verdana, sans-serif",
-            fontSize: 12,
             radius: 23
         };
 
         shield.getShield( options, function ( err, res )
         {
             assert.equal( err, null );
-            assert.equal( res,
-            '<svg xmlns="http://www.w3.org/2000/svg" width="150" height="20">\n\
+            assert.equal( res, '<svg xmlns="http://www.w3.org/2000/svg" width="130" height="20">\n\
     <defs>\n\
         <filter id="drop-shadow">\n\
             <feOffset result="offOut" in="SourceAlpha" dx="0.5" dy="1" />\n\
@@ -223,14 +189,14 @@ describe( "svg-shield", function ()
             <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />\n\
         </filter>\n\
     </defs>\n\
-    <mask id="round-rect"><rect width="150" height="20" rx="10" ry="10" fill="#fff"/></mask>\n\
+    <mask id="round-rect"><rect width="130" height="20" rx="10" ry="10" fill="#fff"/></mask>\n\
     <g mask="url(#round-rect)">\n\
-        <path fill="#333" d="M0 0h100v20H0z"/>\n\
-        <path fill="#000" d="M100 0h60v20H100z"/>\n\
+        <path fill="#555" d="M0 0h90v20H0z"/>\n\
+        <path fill="#4b1" d="M90 0h60v20H90z"/>\n\
     </g>\n\
-    <g fill="#fff" text-anchor="middle" font-family="Verdana, sans-serif" font-size="12">\n\
-        <text x="50" y="14" filter="url(#drop-shadow)">name</text>\n\
-        <text x="125" y="14" filter="url(#drop-shadow)">value</text>\n\
+    <g fill="#fff" text-anchor="middle" font-family="sans-serif" font-size="11">\n\
+        <text x="45" y="14" filter="url(#drop-shadow)"></text>\n\
+        <text x="110" y="14" filter="url(#drop-shadow)"></text>\n\
     </g>\n\
 </svg>' );
 
